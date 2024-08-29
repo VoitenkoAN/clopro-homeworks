f757f3afb3ec314d98840aa865116a749271953a
Voitenko Alex <voitenko_1985@mail.ru>
2024-08-29 18:58:18 +0700

    work3

--- 15.3/sa.tf
+++ 15.3/sa.tf
@@ -1,29 +1,3 @@
-# resource "yandex_iam_service_account" "sa-backet" {
-#   name = "sa-backet"
-# }
-
-# resource "yandex_resourcemanager_folder_iam_member" "sa-backet-editor" {
-#   folder_id = var.folder_id
-#   role      = "storage.editor"
-#   member    = "serviceAccount:${yandex_iam_service_account.sa-backet.id}"
-# }
-
-# resource "yandex_iam_service_account_static_access_key" "sa-backet-static-key" {
-#   service_account_id = yandex_iam_service_account.sa-backet.id
-#   description        = "static access key for object storage"
-# }
-
-# resource "yandex_iam_service_account" "sa-ig1" {
-#   name        = "sa-ig1"
-#   description = "service account to manage IG"
-# }
-
-# resource "yandex_resourcemanager_folder_iam_member" "editor" {
-#   folder_id = var.folder_id
-#   role      = "editor"
-#   member   = "serviceAccount:${yandex_iam_service_account.sa-ig1.id}"
-# }
-
 resource "yandex_iam_service_account" "sa-backet" {
   name = "sa-backet"
 }
